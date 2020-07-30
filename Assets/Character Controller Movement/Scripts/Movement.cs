373e8d62763a1635a0b22a35ad710df2639147a9
Ramon Ferreira <ramon_felipe@outlook.com>
2020-07-29 23:11:05 -0300

    Movement On Character Controller
    Basic and very simple movement on character controller

---
+++ Assets/Character Controller Movement/Scripts/Movement.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+namespace Character_Controller_Movement.Scripts
+{
+    public class Movement : MonoBehaviour
+    {
+        [SerializeField] private CharacterController characterController;
+        [SerializeField] private float speed = 5f;
+
+        [Header("Inputs Values")]
+        [SerializeField] private float horizontalInput;
+        [SerializeField] private float verticalInput;
+
+        [Header("Direction")]
+        [SerializeField] private Vector3 direction;
+        
+        // Start is called before the first frame update
+        private void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            ReceiveInput();
+            CalculateDirection();
+            
+            if(direction.magnitude > 0.1f)
+                Move();
+        }
+
+        private void ReceiveInput()
+        {
+            horizontalInput = Input.GetAxisRaw("Horizontal");
+            verticalInput = Input.GetAxisRaw("Vertical");
+        }
+
+        private void CalculateDirection()
+        {
+            direction = new Vector3(horizontalInput, 0f, verticalInput).normalized;
+        }
+
+        private void Move()
+        {
+            characterController.Move(direction * speed * Time.deltaTime);
+        }
+    }
+}
